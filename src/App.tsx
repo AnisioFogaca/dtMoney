23715fb99d22c554d36d46ad765f0a553cc01855
Gustavo Silva <gustavofsilva@email.com>
2022-02-09 21:11:38 -0300

    fix: fix sintax

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import styled from 'styled-components'
 
-const Title = styled.h1'
-  
+const Title = styled.h1`
   fontsize: 64px;
   color: #8657e6;
-' 
-
+`
 export function App() {
   return (
-    console.log(Title);
     <div className="App">
       <Title>Hello World</Title>
     </div>
